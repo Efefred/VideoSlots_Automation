c00c411c35550784bed8524dc14a6b1c4aa7ca10
Efe Igunbor <Efe Fred Igunbor>
2021-12-21 11:55:20 +0100

    Updates

--- cypress/integration/EvernoteLogin.spec.js
+++ cypress/integration/EvernoteLogin.spec.js
@@ -9,29 +9,43 @@ import testData from '../fixtures/EvernoteLogin.json'
 
 describe('Login Test', () => {
 
-    it('Invalid Email Address', () => {
+    it('User Enters Invalid Credentials and Login Fails', () => {
         
-        //testData.forEach(item=>{
+
          //Visit website
         cy.visit('/');
 
-        //Get Form Fields
-        cy.get('#cf-2').type(item.Email);
-        cy.get('#cf-3').type(item.Mobile);
-        cy.get('#cf-4').type(item.Subject);
-        cy.get('#cf-5').type(item.Message);
-        cy.get('p > .wpcf7-form-control').click();
+        //Invalid Username
+        cy.get('#username').type(testData.username1);
+        cy.get('#loginButton').click();
+        cy.contains(testData['expected_error(invalid_email)'])
 
-        //Assert that the error message, 'The e-mail address entered is invalid.’, is shown 
+        //Invalid Password
+        cy.get('#password').type(testData.password1);
+        cy.get('#loginButton').click();
+        cy.contains(testData['expected_error(Invalid_password)'])
+    })    
+
+
+    it('User Enters Invalid Credentials and Login is Successful', () => {
+
+        cy.get('#username2').type(testData.username2);
+        cy.get('#loginButton').click();
+        cy.contains("Continue")
+        cy.get('#password').type(testData.password2);
+        cy.get('#loginButton').click();
+        cy.contains("Sign in")
+        cy.contains(testData['expected_error(Invalid_password)'])
+
+        //Assert that the text, 'Create new note’, is shown 
         cy.get(':nth-child(4) > .wpcf7-form-control-wrap > .wpcf7-not-valid-tip')
           .should('have.text', 'The e-mail address entered is invalid.')
-        cy.get('#cf-1').clear();
-        cy.get('#cf-2').clear();
-        cy.get('#cf-3').clear();
-        cy.get('#cf-4').clear();
-        cy.get('#cf-5').clear();
-            
-        }) 
+    })  
+    
+      
+        
+  
 
-    }) 
+
+     
 })
